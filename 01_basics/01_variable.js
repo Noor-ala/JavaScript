318f990d206956f4e4e6f480ffb161aec11f8422
Noor-ala <115339852+Noor-ala@users.noreply.github.com>
2023-09-12 11:46:04 +0000

    dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144557;
+let acountEmail = "noor786@gmail.com";
+var acountPassword = "0786";
+accountCity = "Mumbai";
+
+// accountId = 3; //Not allowed;
+acountEmail = "alam678@gmailcom";
+acountPassword = "7860";
+accountCity = "Delhi"; 
+let accountState;
+    
+/*
+prefer not to use var because of issue in block scope and functonal scope
+*/
+
+console.log(accountId);
+console.log([acountEmail,acountPassword,accountCity,accountState]);
